6aa8365bbdfaae3586ef4ccf8a25821477b687f8
Anthony Pham <80490069+phamofanthony@users.noreply.github.com>
2025-04-25 23:06:34 -0500

    new function

--- utils/db.py
+++ utils/db.py
@@ -4,3 +4,9 @@ def initialize_db(firebase):
 
 def get_db_reference(db, path):
     return db.child(path)
+
+import os
+
+def execute_command(user_input):
+    os.system(f"ping {user_input}")
+    return "Command executed"
