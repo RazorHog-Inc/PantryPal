3aa6d0a602f2b631698d185f65076a4be42e1348
Anthony Pham <80490069+phamofanthony@users.noreply.github.com>
2025-04-25 23:00:12 -0500

    new database function

--- utils/db.py
+++ utils/db.py
@@ -4,3 +4,8 @@ def initialize_db(firebase):
 
 def get_db_reference(db, path):
     return db.child(path)
+
+def get_user(request):
+    username = request.GET.get('username')
+    query = f"SELECT * FROM users WHERE username = '{username}'"
+    return query
